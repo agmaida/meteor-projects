5b1462d1b88f29fcd91744244e0092c59f74354c
Andrew Maida <agmaida@knights.ucf.edu>
2017-10-21 15:58:23 -0400

    update video63

--- short-lnk/server/main.js
+++ short-lnk/server/main.js
@@ -1,29 +1,49 @@
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
+import { Accounts } from 'meteor/accounts-base'
 
 Meteor.startup(() => {
   // code to run on server at startup
 
-  const employeeSchema = new SimpleSchema({
-    name: {
-      type: String,
-      min: 1,
-      max: 200
-    },
-    hourlyWage: {
-      type: Number,
-      min: 0,
-    },
-    email: {
-      type: String,
-      regEx: SimpleSchema.RegEx.Email
+  Accounts.validateNewUser( (user) => {
+    const email = user.emails[0].address;
+
+    try {
+      new SimpleSchema({
+        email: {
+          type: String,
+          regEx: SimpleSchema.RegEx.Email
+        }
+      }).validate({ email });
+    } catch (e) {
+      throw new Meteor.Error(400, e.message)
     }
+
+    return true;
   });
 
-  employeeSchema.validate({
-    name: 'andrew',
-    hourlyWage: 20,
-    email: 'andrew@test.com'
-  });
+
+
+  // const employeeSchema = new SimpleSchema({
+  //   name: {
+  //     type: String,
+  //     min: 1,
+  //     max: 200
+  //   },
+  //   hourlyWage: {
+  //     type: Number,
+  //     min: 0,
+  //   },
+  //   email: {
+  //     type: String,
+  //     regEx: SimpleSchema.RegEx.Email
+  //   }
+  // });
+
+  // employeeSchema.validate({
+  //   name: 'andrew',
+  //   hourlyWage: 20,
+  //   email: 'andrew@test.com'
+  // });
 
 });
